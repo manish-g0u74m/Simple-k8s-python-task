6dd71785d10a474aa5f55bea2a77c5736d649c68
Manish Sharma <148465299+manish-g0u74m@users.noreply.github.com>
2025-11-25 11:47:19 +0530

    Update welcome message in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "<h1>Welcome to the Kainskep Service!</h1>"
+    return "<h1>Welcome to the Kainskep Solutions!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
